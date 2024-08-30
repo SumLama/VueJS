740e56bc273c50621221dcdc920d6c4e7e4854ac
Sumitra Tamang <sumitratamang@2400-1A00-B050-ip6.wlink.com.np>
2024-08-30 17:45:17 +0545

    feat:styling of login page, registered data stored in local storage

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,131 +1,178 @@
 <script setup>
 import { ref, reactive } from 'vue';
-
+import Vbind from './Vbind.vue';
 
 const user = reactive({
   fname: '',
   email: '',
   password: '',
-  confirmpassword: ''
+  confirmpassword: '',
 });
 
 
-const errors = reactive({
+const errors =reactive({
   fname: false,
   email: false,
   password: false,
-  confirmpassword: false
+  confirmpassword: false,
+  Invalid:false
+ 
 });
-
+const validateUser=()=>{
+  errors.fname = false;
+  errors.email = false;
+  errors.password = false;
+  errors.confirmpassword = false;
+  errors.Invalid = false;
+ 
+  if(user.fname ==''){
+      errors.fname=true
+  }
+  if(user.email ==''){
+      errors.email=true
+  }
+  if(user.password==''){
+      errors.password=true
+  }
+  if(user.password!==user.confirmpassword){
+      errors.confirmpassword=true
+  }
+  if(errors.fname || errors.email || errors.password || errors.confirmpassword){
+    errors.Invalid = true
+  }
+  return 
+}
 
 const onSubmit = () => {
+  validateUser()
+  if(!errors.Invalid){
+  const userData = {
+    name:user.fname,
+    email:user.email,
+    password:user.password
+  }
+  let users = JSON.parse(localStorage.getItem('users',))||[]
+  console.log('Users ', users);
 
+  users.push(userData);
+  localStorage.setItem('users',JSON.stringify(users))
+  alert('User registered successfully!');  
  
-    errors.fname = false;
-    errors.email = false;
-    errors.password = false;
-    errors.confirmpassword = false;
-    if(user.fname==''){
-        errors.fname=true
-    }
-    if(user.email==''){
-        errors.email=true
-    }
-    if(user.password==''){
-        errors.password=true
-    }
-    if(user.password!=user.confirmpassword){
-        errors.confirmpassword=true
-    }
-
-   
-
+  user.fname = '';
+  user.email = '';
+  user.password = '';
+  user.confirmpassword = '';
+  errors.Invalid=false;
  
+  } else {
+    
+    alert('Cannot regsiter....Try again');
+  }
+  const testalert = ()=>{
+    alert("hello");
+
+  }
+
 }
 </script>
 
 <template>
   <div class="container">
     <div class="form-container">
-    <h2 style="text-align: center">Register</h2>
+    <h2 style="text-align: center">Sign Up</h2>
     <form @submit.prevent="onSubmit">
       <div class="form-field"> 
         <label>Fullname</label><br/>
-        <input type="text" placeholder="Fullname" v-model="user.fname"/>
+        <input type="text"  v-model="user.fname"/>
       </div>
-      <div v-if="errors.fname" class="error-message"><p>Fullname is required</p></div>
+      <div v-if="errors.fname && !user.fname"  class="error-message"><p>Fullname is required</p></div>
       
       <div class="form-field">
         <label>Email</label><br/>
-        <input type="email" placeholder="Email" v-model="user.email"/>
+        <input type="email"  v-model="user.email"/>
       </div>
-      <div v-if="errors.email" class="error-message"><p>Email is required</p></div>
+      <div v-if="errors.email && !user.email" class="error-message"><p>Email is required</p></div>
       
       <div class="form-field">
         <label>Password</label><br/>
-        <input type="password" placeholder="Password" v-model="user.password"/>
+        <input type="password" v-model="user.password"/>
       </div>
-      <div v-if="errors.password" class="error-message"><p>Password is required</p></div>
+      <div v-if="errors.password && !user.password" class="error-message"><p>Password is required</p></div>
       
       <div class="form-field">
         <label>Confirm Password</label><br/>
-        <input type="password" placeholder="Confirm Password" v-model="user.confirmpassword"/>
+        <input type="password" v-model="user.confirmpassword"/>
       </div>
-      <div v-if="errors.confirmpassword"class="error-message"><p>Passwords do not match</p></div>
-      <div >
-      <button type="submit" class="btn">Register</button>
+      <div v-if="errors.confirmpassword && !user.password " class="error-message"><p>Passwords do not match</p></div>
+      <div class="button-control" >
+      <button type="submit">Sign up</button>
     </div>
+    <div class="text-control">Already have an account?<a href="#">Sign in</a></div>
+   
     </form>
+   
   </div>
   </div>
 </template>
 
 <style scoped>
-   .container{
+  .container{
     height: 100vh;
     display: flex;
     justify-content: center;
-    align-items: center;
-    
-   }
-   .form-container{
+    align-items: center; 
+  }
+
+  .form-container{
     background-color: rgb(230, 229, 229);
     padding:1.7rem;
     padding-top: 0;
     border: 1px rgb(127, 121, 121);
     border-radius:10px;
     box-shadow: 3px rosybrown;
-
-    
    }
-   .form-field{
+
+  .form-field{
     margin-top: 12px;
-   }
-    .form-field label{
-        font-size:1rem; 
-        
-        
-    }
-    .form-field input{
-        height:2.25rem;
-        width:20rem;
-        margin-top: 0.3rem;
-        border-radius: 0.2rem;
-        box-shadow: 3px;
-    }
-    .error-message{
-        color:red;
-        margin-top: 0px
-    }
+  }
 
-    .btn{
-        margin-top: 1rem;
-        width:6rem;
-        padding:8px;
-        font-size: 0.9rem;
-        border-radius: 5px;
-        background-color:#9cb9b5;
-        cursor: pointer;
-        
-    }
+  .form-field label{
+    font-size:1rem;     
+  }
+
+  .form-field input{
+    height:2.25rem;
+    width:20rem;
+    margin-top: 0.3rem;
+    border-radius: 0.2rem;
+    box-shadow: 3px;
+    border: none;
+  }
+
+  .error-message{
+    color:red;
+    margin-top: 0px
+  }
+
+  .button-control{
+    /* display: flex;
+    justify-content: center; */
+    text-align: center;
+  }
+
+  .button-control button{
+    margin: 1.5rem;
+    /* width:6rem; */
+    padding:10px 30px;
+    font-size: 1rem;
+    border-radius: 8px;
+    background-color:#9cb9b5;
+    cursor: pointer;
+    border: none;    
+  }
+  .text-control{
+    font-size: small;
+    display: flex;
+    justify-content: center;
+  }
 </style>
--- src/components/Vbind.vue
+++ src/components/Vbind.vue
@@ -1,51 +1,96 @@
 <script setup>
-import { ref } from 'vue';
-const credentials ={ 
-    email:"user@gmail.com",
-    password:"user123"
+import { reactive, ref } from 'vue';
+
+const user = reactive({
+    email: '',
+    password:''
+})
+
+const Login = ()=>{
+    let users = JSON.parse(localStorage.getItem('users'));
+    
+    users.forEach(u => {
+     
+        if (u.email == user.email && u.password== user.password){
+            console.log("hello")
+            alert("login successfull")
+     }
+     else{
+        alert("Invalid email or password")
+     }
+   
+
+    }); 
+
+     
 
 }
-const email = ref('');
-const password = ref('');
-const IsLoggedin =ref(false)
- const ValidateUser=()=>{
-    if(email.value ==credentials.email && password.value == credentials.password){
-        IsLoggedin.value = true;
-        alert("Login successfull");
-    }
-    else{
-        alert("Error occured");
-    }
-   
- }
- 
-
 
 </script>
 <template>
     <div class="container">
-        <div v-if="!IsLoggedin">
-            <form name="loginForm" @submit.prevent="ValidateUser()" >
-                <div class="form-control">
-                    <label>Email:</label>
-                    <input type="email" name="email" placeholder="Enter your email"  v-model="email">
+        <div class="form-container" >
+            <h2 style="text-align: center;"> Sign In</h2>
+            <form name="loginForm" @submit.prevent="Login" >
+                <div class="form-field"> 
+                    <label>Email</label><br/>
+                    <input type="email" name="email" v-model="user.email" required/>
                 </div>
-                <div class="form-control">
-                    <label>password:</label>
-                    <input type="password" name="passoword" placeholder="Enter your password" v-model="password"/>
+                <div class="form-field"> 
+                    <label>password</label><br/>
+                    <input type="password" name="password" v-model="user.password" required/>
                 </div>
-                <div>
-                    <button>Login</button>
+                <div class="button-control">
+                    <button >Sign in</button>
                 </div>
             </form>
         </div>
-        <div v-else>
-        <p>Hello welcome</p>
     </div>
-    </div>
-  
-
 </template>
 <style scoped>
+.container{
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+}
+.form-container{
+    background-color: rgb(230, 229, 229);
+    padding:1.7rem;
+    padding-top: 0;
+    border: 1px rgb(127, 121, 121);
+    border-radius:10px;
+    box-shadow: 3px rosybrown;
+}
+.form-field{
+    margin-top: 12px;
+}
+.form-field label{
+    font-size:1rem; 
+               
+}
+.form-field input{
+    height:2.25rem;
+    width:20rem;
+    margin-top: 0.3rem;
+    border-radius: 0.2rem;
+    box-shadow: 3px;
+    border: none;
+}
+.button-control{
+    text-align: center;
+}
 
+.button-control button{
+    margin: 1.5rem;
+    /* width:6rem; */
+    padding:10px 30px;
+    font-size: 1rem;
+    border-radius: 8px;
+    background-color:#9cb9b5;
+    cursor: pointer; 
+    border: none;
+        
+}
 </style>
